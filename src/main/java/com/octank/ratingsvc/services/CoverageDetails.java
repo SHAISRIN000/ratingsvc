ec84baea49f2d890b0ffb939a60294a884a54c94
shaiksri <shaiksri@192.168.2.186>
2021-05-20 15:30:11 -0400

    wer

---
+++ src/main/java/com/octank/ratingsvc/services/CoverageDetails.java
@@ -0,0 +1,29 @@
+package com.octank.ratingsvc.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CoverageDetails {
+
+	List<Coverage> coverages=new ArrayList<>();
+	
+    private Integer policyNumber;
+
+
+	public Integer getPolicyNumber() {
+		return policyNumber;
+	}
+
+	public void setPolicyNumber(Integer policyNumber) {
+		this.policyNumber = policyNumber;
+	}
+
+	public List<Coverage> getCoverages() {
+		return coverages;
+	}
+
+	public void setCoverages(List<Coverage> coverages) {
+		this.coverages = coverages;
+	}
+	
+}
